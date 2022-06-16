d418b21ae4ca04df337707b8a49d0dc6b4f65f0a
Tamas Pop <poptamas91@yahoo.com>
2022-06-16 15:43:43 +0300

    Updates

---
+++ precache-manifest.89e8ba97cb1eed9e2b53579f4bcbcc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5327bed2ef8d588460c9897732161f03",
+    "url": "/Cat-portal/index.html"
+  },
+  {
+    "revision": "4bdfba7593aa77f6e5b1",
+    "url": "/Cat-portal/static/css/2.c0cdccd9.chunk.css"
+  },
+  {
+    "revision": "4bdfba7593aa77f6e5b1",
+    "url": "/Cat-portal/static/js/2.c81415ba.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Cat-portal/static/js/2.c81415ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362ed056b1db283ee1de",
+    "url": "/Cat-portal/static/js/main.8624e4c4.chunk.js"
+  },
+  {
+    "revision": "770444a7eb46cb60a194",
+    "url": "/Cat-portal/static/js/runtime-main.196bf52d.js"
+  }
+]);
